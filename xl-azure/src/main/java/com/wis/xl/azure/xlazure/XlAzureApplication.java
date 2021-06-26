eedb6184f9cce1385e004fd746734a685bf44c2f
Mohamed Arifein <arif.mohammed>
2021-06-26 15:47:26 +0530

    first commit

---
+++ xl-azure/src/main/java/com/wis/xl/azure/xlazure/XlAzureApplication.java
@@ -0,0 +1,14 @@
+package com.wis.xl.azure.xlazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XlAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XlAzureApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ xl-azure/src/test/java/com/wis/xl/azure/xlazure/XlAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wis.xl.azure.xlazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XlAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
